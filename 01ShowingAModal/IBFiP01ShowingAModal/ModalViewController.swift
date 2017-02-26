bcf5fd272ae7700daced0f929fedf7129ed29721
John L. Stricker <john.stricker@raizlabs.com>
2017-02-26 13:48:23 -0600

    removing ModalView & incorporating it into ModalViewController

--- 01ShowingAModal/IBFiP01ShowingAModal/ModalViewController.swift
+++ 01ShowingAModal/IBFiP01ShowingAModal/ModalViewController.swift
@@ -6,12 +6,27 @@
 //  Copyright © 2016 Raizlabs. All rights reserved.
 //
 
+import Anchorage
 import UIKit
 
 /// A modal with dark blurred background and light UIStatusBar
 class ModalViewController: UIViewController {
 
-    // MARK - UIViewController var overrides
+    fileprivate let blurView: UIVisualEffectView = {
+        let effect = UIBlurEffect(style: UIBlurEffectStyle.dark)
+        return UIVisualEffectView(effect: effect)
+    }()
+
+    fileprivate let dismissButton = DismissButton()
+
+    override func viewDidLoad() {
+        configureActions()
+        configureView()
+    }
+}
+
+// MARK: - UIViewContoller style overrides
+extension ModalViewController {
 
     override var modalPresentationStyle: UIModalPresentationStyle {
         get {
@@ -40,22 +55,48 @@ class ModalViewController: UIViewController {
         return .lightContent
     }
 
-    // MARK: - View lifecycle
-
-    override func loadView() {
-        view = ModalView()
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        (view as? ModalView)?.delegate = self
-    }
 }
 
-// MARK: - ModalViewDelegate
-extension ModalViewController: ModalViewDelegate {
-    
-    func modalDismissButtonTapped(_ modalView: ModalView) {
+// MARK: - Actions
+private extension ModalViewController {
+
+    func configureActions() {
+        dismissButton.addTarget(self, action: #selector(dismissButtonTapped), for: .touchUpInside)
+    }
+
+    @objc func dismissButtonTapped() {
         presentingViewController?.dismiss(animated: true, completion: nil)
+
     }
 }
+
+// MARK: - View Configuration
+private extension ModalViewController {
+
+    enum Constants {
+        static let dismissButtonMarginRight = CGFloat(10)
+        static let dismissButtonMarginTop = CGFloat(30)
+    }
+
+    func configureView() {
+
+        // View Hierarchy
+        view.addSubview(blurView)
+        view.addSubview(dismissButton)
+
+        // Style
+        view.isOpaque = false
+        view.backgroundColor = AppColor.clear
+
+        // Layout
+
+        // blurView shares all edges w/its parent
+        blurView.edgeAnchors == view.edgeAnchors
+
+        // dismissButton is placed at the top right
+        dismissButton.topAnchor == view.topAnchor + Constants.dismissButtonMarginTop
+        dismissButton.rightAnchor == view.rightAnchor - Constants.dismissButtonMarginRight
+    }
+
+}
+
